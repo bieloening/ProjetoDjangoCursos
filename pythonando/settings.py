fbec061f3b89f4bbec85d2b0dc4c1ca11dc8e1e6
bieloening <oening700@gmail.com>
2024-04-09 18:29:02 -0300

    atualização no models - inclusão de imagens e descrição dos cursos na home

--- pythonando/settings.py
+++ pythonando/settings.py
@@ -123,8 +123,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cursos/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Media files (Uploaded files)
 # https://docs.djangoproject.com/en/5.0/topics/files/
 

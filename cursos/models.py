f0b2e35dc4a9c2826de785f367204bc16b023f52
bieloening <oening700@gmail.com>
2024-04-09 18:26:48 -0300

    atualização no models - inclusão de imagens e descrição dos cursos na home

--- cursos/models.py
+++ cursos/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-class  Curso(models.Model):
+class Curso(models.Model):
     nome = models.CharField(max_length=40)
     carga_horaria = models.IntegerField()
     data_criacao = models.DateTimeField()
     ativo = models.BooleanField(default=True)
+    imagem = models.ImageField(upload_to='images/')
+    descricao = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
